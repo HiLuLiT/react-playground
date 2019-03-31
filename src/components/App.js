4247e09d2ae91aeeeec953bacc595df83682ade0
hila.ben-baruch <hila.ben-baruch@ironsrc.com>
2019-03-31 15:21:37 +0300

    added header with navigation links

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import HomePage from './home/HomePage'
 import AboutPage from './about/AboutPage'
+import Header from './common/Header'
 
 function App() {
 	return (
 		<div className="container-fluid">
-			{/*// 'exact' prop means only the empty route matches, otherwise this will match any other routes since / is in any route.*/}
+			{/*React-router will watch the URL and render the proper route (home/about, based on line 14-15)*/}
+			{/*Our header will always display above*/}
+			<Header/>
+			{/*'exact' prop means only the empty route matches, otherwise this will match any other routes since / is in any route.*/}
 			<Route exact path="/" component={HomePage}/>
 			<Route path="/about" component={AboutPage}/>
 		</div>
---
+++ src/components/common/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// NavLink is an anchor that react router controls
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+	const activeStyle = {color: '#F15B2A'}
+	
+	return (
+		<nav>
+			{/*this link will only recieve the activeStyle when on the homepage*/}
+			<NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>{" | "}
+			<NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
+		</nav>
+	)
+}
+
+export default Header
