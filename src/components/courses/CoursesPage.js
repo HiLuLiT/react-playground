f9a2743c14802b40cd4deb8f26e47d71c8fdd1c4
hila.ben-baruch <hila.ben-baruch@ironsrc.com>
2019-04-03 14:16:47 +0300

    added simple course Form with method binding

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,35 @@
 import React from 'react'
 
 class CoursesPage extends React.Component {
+	constructor(props) {
+		super(props)
+		
+		this.state = {
+			course: {
+				title: ""
+			}
+		}
+	}
+	
+	handleChange(event) {
+		// create a copy of our course object that refelcts the new title
+		// there's an issue - our function is inheriting the this context of the caller (the change handler). we need to bind this to our instance.
+		const course = { ...this.state.course, title: event.target.value }
+		this.setState({
+			// object shorthand syntax - if right equals left (course: course)
+			course
+		})
+	}
 	render() {
-		return <h2>Courses</h2>
+		return (
+			<form>
+				<h2>Courses</h2>
+				<h3>Add Course</h3>
+				{/*we could bind here: this.handleChange.bind(this), but it's not ideal since a new func is allocated on every render*/}
+				<input type="text" onChange={this.handleChange} value={this.state.course.title}/>
+				<input type="submit" onChange={this.handleChange} value="Save"/>
+			</form>
+		)
 	}
 }
 
