12b304f4be6ece7e1162a33245572201249f9e55
hila.ben-baruch <hila.ben-baruch@ironsrc.com>
2019-04-04 01:01:34 +0300

    4th way best practice to use mapDispatchToProps - object form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {connect} from 'react-redux'
 import * as courseActions from '../../redux/actions/courseActions'
 import PropTypes from 'prop-types'
-import {bindActionCreators} from 'redux'
 
 class CoursesPage extends React.Component {
 		state = {
@@ -18,7 +17,7 @@ class CoursesPage extends React.Component {
 	
 	handleSubmit = event => {
 		event.preventDefault();
-		this.props.actions.createCourse(this.state.course)
+		this.props.createCourse(this.state.course)
 	}
 	
 	render() {
@@ -38,7 +37,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
 	courses: PropTypes.array.isRequired,
-	actions: PropTypes.object.isRequired
+	createCourse: PropTypes.func.isRequired
 }
 
 
@@ -47,13 +46,11 @@ function mapStateToProps(state) {
 		courses: state.courses
 	}
 }
-// redux comes with a helper function to save us from have to manually wrap our action creators in a dispatch call
-// this function is called bindActionCreators
-function mapDispatchToProps(dispatch) {
-	return {
-		// this one line ends up wrapping all of our course actions -
-		// right now it has only one, but if we add more no need to change this line.
-		actions: bindActionCreators(courseActions, dispatch)
-	}
+
+// 4th option - declare mapDispatchToProps as an object
+// when declared as an object, each property is automatically bound to dispatch
+const mapDispatchToProps =  {
+	//inside this object, we create property for each action that we want to expose
+		createCourse: courseActions.createCourse
 }
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage)
