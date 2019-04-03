f87c48be80b9ee3489d18f13d6df72dd4cf61622
hila.ben-baruch <hila.ben-baruch@ironsrc.com>
2019-04-04 00:17:10 +0300

    second way to use mapDispatchToProps - Manual Mapping

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,7 @@
 import React from 'react'
-// let's connect CoursesPage to Redux!
 import {connect} from 'react-redux'
 import * as courseActions from '../../redux/actions/courseActions'
 import PropTypes from 'prop-types'
-
 
 class CoursesPage extends React.Component {
 		state = {
@@ -18,11 +16,8 @@ class CoursesPage extends React.Component {
 	}
 	
 	handleSubmit = event => {
-			// prevent the form from causing the page to reload
 		event.preventDefault();
-		// The ugly way first - dispatching an action without using mapDispatchToProps as one of connect's parameters.
-		// since we didn't declare mapDispatchToProps, connect automatically adds Dispatch as a prop.
-		this.props.dispatch(courseActions.createCourse(this.state.course))
+		this.props.createCourse(this.state.course)
 	}
 	
 	render() {
@@ -32,8 +27,6 @@ class CoursesPage extends React.Component {
 				<h3>Add Course</h3>
 				<input type="text" onChange={this.handleChange} value={this.state.course.title}/>
 				<input type="submit" onChange={this.handleChange} value="Save"/>
-				{/*display list of courses from redux store*/}
-				{/*concise arrow syntax - we can ommit the return keyword cause we wrapped our expression in parentheses*/}
 				{this.props.courses.map(course => (
 					<div key={course.title}>{course.title}</div>
 				))}
@@ -44,19 +37,26 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
 	courses: PropTypes.array.isRequired,
-	dispatch: PropTypes.func.isRequired
+	// since we declared mapDispatchToProps, dispatch is no longer injected.
+	// Only the actions we declared in mapDispatchToProps are passed in.
+	createCourse: PropTypes.func.isRequired
 }
 
 
-//This func determines what state is passed to our component via props.
-// it has 2 arguments - state, and ownProps. we'll use ownProps later (it gives us access to the props that are being attached to the component)
-function mapStateToProps(state, ownProps) {
+function mapStateToProps(state) {
 	return {
-		// be specific - request only the data your component needs.
 		courses: state.courses
 	}
 }
 
-// the 2 parantheses ()() - this is 2 function calls. the connect function returns a function, that function then calls our component.
-export default connect(mapStateToProps)(CoursesPage)
-// mapDispatchToProps parameter is optional, when we omit it, our component gets a dispatch prop injected automatically so we can dispatch actions.
+// second way to use mapDispatchToProps - wrap it Manually
+// recives dispatch as sole argument
+// the actions we choose to return here will be available on props
+function mapDispatchToProps(dispatch) {
+	return {
+		// here we pass our action creators to dispatch, to fire the action.
+		// remember if you don't call dispatch nothing happens. action creators must be called by dispatch.
+		createCourse: course => dispatch(courseActions.createCourse(course))
+	}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage)
